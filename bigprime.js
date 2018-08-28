c47bf9339395635884342eadc01d3fd254496363
Mike Nickaloff <mike@datafault.net>
2018-08-28 08:34:34 -0700

    Update bigprime.js

--- bigprime.js
+++ bigprime.js
@@ -1,1 +1,7 @@
- function isBigPrime(istr){var i=BigInt(istr); var n=i%78n;return!(![3n,5n,7n,11n,13n,17n,19n,23n,29n,31n,37n,41n,43n,47n,53n,59n,61n,67n,71n,73n].includes(n)&&1n!=n&&![4n,9n,16n,25n,36n,49n,64n].includes(n)||i%5n==0n||i%3n==0n||i%7n==0||i%11n==0n||i%n==0n)}
+//usage is isHugePrime(str)
+// tests if the given string represents a prime number
+//
+// example:  isHugePrime("38383883838838383838383736476374637432")
+// returns:   false
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('K J(i){3 x=c(i);3 e=[E,G,I,F,9,B,w];3 5=[f,h,g,d,C,v,z,u,s,m,l,n,o,p,A,P,D,M,N,O];3 y=c(x%b);2(5.8(x)){6 7}2((5.8(y))||(y==c(1))||(e.8(y))){2((x%h)!==4){2((x%f)!==4){2((x%g)!==4){2((x%d)!==4){2((y!==9)&&((x%y)!==4)){3 q=x;H((q-b)>=4){q-=b}2((L>q)&&(q>4)){6 7}q=x;j(3 t=0;t<5.k;t++){3 r=5[t];2((q%r)===4){6 a}}6 7}2(y===9){3 q=x;j(3 t=0;t<5.k;t++){3 r=5[t];2((q%r)===4){6 a}}6 7}}}}}}6 a}',52,52,'||if|var|0n|prime_nums|return|true|includes|1n|false|78n|BigInt|11n|square_nums|3n|7n|5n|inp_x|for|length|37n|31n|41n|43n|47n|||29n||23n|17n|55n|||19n|53n|35n|13n|61n|9n|77n|25n|while|49n|isHugePrime|function|2n|67n|71n|73n|59n'.split('|'),0,{}))
